c3c2cb4c907dbc8dd2be3598135f863c27cd8959
Emmanuel Asu <mannyasu2@gmail.com>
2019-02-11 12:39:22 -0500

    shortlist resume initial

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
 #! /usr/bin/env node
 
-const fs = require('fs')
+const fs = require("fs");
 
-fs.readFile(__dirname + '/info.json', 'utf8', function (err, data) {
-    if (err) {
-        console.log(err)
-    } else {
-        console.log(data)
-        return data
-    }
-})
-"bin": {
-    "your-command-here": "./index.js"
-},
+fs.readFile(__dirname + "/info.json", "utf8", function(err, data) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
+    return data;
+  }
+});
